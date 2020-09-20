47060eac915b5061fe4a19f39bae317738163b4f
git9527 <zhangshaoneng@etradeunion.com>
2020-09-20 18:36:31 +0800

    add view base

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
 // https://router.vuejs.org/zh/guide/essentials/history-mode.html#%E5%90%8E%E7%AB%AF%E9%85%8D%E7%BD%AE%E4%BE%8B%E5%AD%90
 const router = new VueRouter({
   mode: "history",
-  base: "view",
+  base: "/view/",
   routes
 });
 
